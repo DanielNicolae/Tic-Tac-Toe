c5e8821486c2038915b4df7c1624a5965e7b9ed9
DanielNicolae <deadlypoet@protonmail.com>
2021-04-09 22:15:52 +0300

    Adding the 3/3 grid.

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-"use strict";
+// "use strict";
 
+// import grid from "grid";
+
+// const xAndO = new grid;
+
+// console.log(xAndO);
+
+const grid = (function () {
+  return {
+    x0y0: null, x1y0: null, x2y0: null,
+    x0y1: null, x1y1: null, x2y1: null,
+    x0y2: null, x1y2: null, x2y2: null
+  };
+})();
+
+console.log(grid)
