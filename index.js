254a07cb57c633c7fdb97b2ac02077fa8be393c8
DanielNicolae <deadlypoet@protonmail.com>
2021-04-12 02:56:51 +0300

    Displaying the winner in the console.

--- index.js
+++ index.js
@@ -1,31 +1,60 @@
 "use strict";
 
 const grid = (function () {
-  const XorO = {
+  const x0y0 = document.getElementById("x0y0");
+  const x1y0 = document.getElementById("x1y0");
+  const x2y0 = document.getElementById("x2y0");
+  const x0y1 = document.getElementById("x0y1");
+  const x1y1 = document.getElementById("x1y1");
+  const x2y1 = document.getElementById("x2y1");
+  const x0y2 = document.getElementById("x0y2");
+  const x1y2 = document.getElementById("x1y2");
+  const x2y2 = document.getElementById("x2y2");
+
+  let XorO = {
     x0y0: null, x1y0: null, x2y0: null,
     x0y1: null, x1y1: null, x2y1: null,
     x0y2: null, x1y2: null, x2y2: null
   };
-  let playerX = true;
+
+  let isPlayerX = true;
   const inputXorO = function (e) {
     const box = e.target;
     if (box.tagName === "TD" && (box.textContent !== "X" && box.textContent !== "O")) {
-      if (playerX) {
+      if (isPlayerX) {
         box.textContent = "X";
-        playerX = false;
+        isPlayerX = false;
       } else {
         box.textContent = "O";
-        playerX = true;
+        isPlayerX = true;
       }
     }
-  }
+    displayWinner(e);
+  };
+
+  function displayWinner() {
+    if (x0y0.textContent !== "#" && x0y0.textContent !== "" &&
+      x1y0.textContent !== "#" && x1y0.textContent !== "" &&
+      x2y0.textContent !== "#" && x2y0.textContent !== "") {
+      if ((x0y0.textContent === x1y0.textContent && x1y0.textContent === x2y0.textContent) ||
+        (x0y1.textContent === x1y1.textContent && x1y1.textContent === x2y1.textContent) ||
+        (x0y2.textContent === x1y2.textContent && x1y2.textContent === x2y2.textContent) ||
+        (x0y0.textContent === x0y1.textContent && x0y1.textContent === x0y2.textContent) ||
+        (x1y0.textContent === x1y1.textContent && x1y1.textContent === x1y2.textContent) ||
+        (x2y0.textContent === x2y1.textContent && x2y1.textContent === x2y2.textContent) ||
+        (x0y0.textContent === x1y1.textContent && x1y1.textContent === x2y2.textContent) ||
+        (x2y0.textContent === x1y1.textContent && x1y1.textContent === x0y2.textContent)) {
+        console.log(`Player ${x0y0.textContent} won!`);
+      }
+    }
+
+  };
+
   return {
-    XorO,
-    inputXorO
+    inputXorO,
+    displayWinner
   };
 })();
-
-let ticTacToeGame = grid.XorO;
 
 
 function checkPlayerInput() {
