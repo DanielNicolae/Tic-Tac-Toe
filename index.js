d726a876720009c7ad0c72fca97ad5536fe971d1
DanielNicolae <deadlypoet@protonmail.com>
2021-04-12 01:57:48 +0300

    Added the input for the tic tac toe game.

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-// "use strict";
-
-// import grid from "grid";
-
-// const xAndO = new grid;
-
-// console.log(xAndO);
+"use strict";
 
 const grid = (function () {
-  return {
+  const XorO = {
     x0y0: null, x1y0: null, x2y0: null,
     x0y1: null, x1y1: null, x2y1: null,
     x0y2: null, x1y2: null, x2y2: null
   };
+  let playerX = true;
+  const inputXorO = function (e) {
+    const box = e.target;
+    if (box.tagName === "TD" && (box.textContent !== "X" && box.textContent !== "O")) {
+      if (playerX) {
+        box.textContent = "X";
+        playerX = false;
+      } else {
+        box.textContent = "O";
+        playerX = true;
+      }
+    }
+  }
+  return {
+    XorO,
+    inputXorO
+  };
 })();
 
-console.log(grid)
+let ticTacToeGame = grid.XorO;
+
+
+function checkPlayerInput() {
+  document.getElementById("tictactoe").addEventListener("click", grid.inputXorO);
+}
+
+checkPlayerInput();
